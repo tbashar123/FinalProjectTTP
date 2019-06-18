ba0e0f57b6cd4384bfa6e5c44042c764d893036b
Tanvir Bashar <tbashar09@gmail.com>
2019-06-18 00:06:17 -0400

    new design

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link,  Switch  } from 'react-router-dom'
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const routing = (
+
+<Router>
+    <div>
+
+    <ul id="nav">
+         <li><Link to="/">Home</Link></li>
+
+
+              </ul>
+
+              <Switch>
+                  <Route exact path="/" component={App} />
+
+
+              </Switch>
+
+              </div>
+</Router>
+
+);
+
+
+ReactDOM.render( routing,  document.getElementById('root'));//<Provider store={reduxStore}>
+//  <App />
+//   </Provider>,
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
