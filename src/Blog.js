e0cd712a318b17233aa4dd2db0aa584d0fb6d6b3
Tanvir Bashar <tbashar09@gmail.com>
2019-06-18 15:55:54 -0400

    changes to blog

--- src/Blog.js
+++ src/Blog.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import Form from './Form'
 
 class Blog extends React.Component {
-
+  render() {
+      return (
+        <div className="container">
+        <div className="row pt-5">
+          <div className="col-12 col-lg-6 offset-lg-3">
+            <h1 className="text-center">LightBlog</h1>
+          </div>
+          <Form />
+        </div>
+      </div>
+      );
+}
 
 }
 
